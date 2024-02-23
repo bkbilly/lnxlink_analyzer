41bd8d955139758794c8f6322aa33d7f1f1fccfd
bkbilly <bkbilly@hotmail.com>
2024-02-23 22:02:42 +0200

    Add LNXlink API

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+// https://developers.cloudflare.com/d1/get-started/
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+  // If you set another name in wrangler.toml as the value for 'binding',
+  // replace "DB" with the variable name you defined.
+  DB: D1Database;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env) {
+    const { pathname } = new URL(request.url);
+
+    if (pathname.startsWith("/api/lnxlink")) {
+      console.log(request.cf.country)
+
+      if (request.method === "POST") {
+        const data = await request.json();
+        console.log(data);
+        await env.DB.prepare(
+            "INSERT INTO LNXlink (uuid, version, country) VALUES (?, ?, ?)"
+          )
+            .bind(data.uuid, data.version, request.cf.country)
+            .all();
+            return new Response(null)
+      } else if (pathname.endsWith("users")) {
+        const { results } = await env.DB.prepare(
+          "SELECT date(created) as date, count(DISTINCT(uuid)) as sum, version FROM LNXlink group by date(created), version"
+        ).all();
+        return Response.json(results);
+      }  else if (pathname.endsWith("countries")) {
+        const { results } = await env.DB.prepare(
+          "SELECT date(created) as date, count(DISTINCT(uuid)) as sum, country FROM LNXlink group by date(created), country"
+        ).all();
+        return Response.json(results);
+      }
+    }
+
+    return new Response(null, { status: 405 });
+  },
 };
